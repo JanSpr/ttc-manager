b85f0b766a321e2b68e70fd399a762a765df0b9d
Janek <jsprengart@uni-koblenz.de>
2026-04-21 17:31:00 +0200

    first step vor management-integration on members

--- frontend/src/types/member.ts
+++ frontend/src/types/member.ts
@@ -1,9 +1,20 @@
+export type MemberType = "ADULT" | "YOUTH";
+
 export type Member = {
   id: number;
   firstName: string;
   lastName: string;
   fullName: string;
   active: boolean;
+  type: MemberType;
   userId: number | null;
   teamIds: number[];
+};
+
+export type MemberUpsertRequest = {
+  firstName: string;
+  lastName: string;
+  active: boolean;
+  type: MemberType;
+  userId: number | null;
 };
